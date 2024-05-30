11d72a90f0373837868bcda84e184b7ee324c33e
Suraj Shah <surajshah.bombay@gmail.com>
2024-05-30 10:21:35 +0530

    Added port

--- main.py
+++ main.py
@@ -87,3 +87,8 @@ async def predict(file: UploadFile = File(...)):
         return{"Voice" : f"AI with probablilty: {final_result}"}
     else:
         return{"Voice" : f"Real with probablilty: {1 - final_result}"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
